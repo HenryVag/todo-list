3d95696bd58ca78c3252529cbd92bb9fad586bf8
Henry <henry.vag@edu.turkuamk.fi>
2024-11-14 14:24:14 +0200

    user controller with all users view + extra stuff

---
+++ controllers/usersController.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const User = require("../models/user");
+
+module.exports = {
+  index: (req, res, next) => {
+    User.find()
+      .then((users) => {
+        res.locals.users = users;
+        next();
+      })
+      .catch((error) => {
+        console.log(`Error fetching users: ${error.message}`);
+        next(error);
+      });
+  },
+
+  indexView: (req, res) => {
+    res.render("users/index");
+  } /*
+  new: (req, res) => {
+    res.render("users/new");
+  },
+  create: (req, res, next) => {
+    if (req.skip) next();
+    let userParams = {
+      name: {
+        first: req.body.first,
+        last: req.body.last,
+      },
+      email: req.body.email,
+      zipCode: req.body.zipCode,
+      phoneNumber: req.body.phoneNumber,
+    };
+    let newUser = new User(userParams);
+
+    User.register(newUser, req.body.password, (error, user) => {
+      if (user) {
+        req.flash(
+          "success",
+          `${user.first} ${user.last}'s account created succesfully!`
+        );
+        res.locals.redirect = "/users";
+        next();
+      } else {
+        req.flash(
+          "error",
+          `Failed to create user account because ${error.message}`
+        );
+        res.locals.redirect = "/users/new";
+        next();
+      }
+    });
+  },*/,
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath) res.redirect(redirectPath);
+    else next();
+  },
+};
